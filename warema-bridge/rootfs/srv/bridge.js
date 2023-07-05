069fb7607fa300d43fc20cce5f94d84a0347f913
fisch55 <42477687+fisch55@users.noreply.github.com>
2023-07-05 14:03:55 +0200

    Update bridge.js

--- warema-bridge/rootfs/srv/bridge.js
+++ warema-bridge/rootfs/srv/bridge.js
@@ -136,7 +136,10 @@ function registerDevice(element) {
 
 function registerDevices() {
   registerDevice({snr: 00994624, name:"Markise"  , type:25 })
-  registerDevice({snr: 01140790, name:"AwningW", type:20})
+  registerDevice({snr: 01140790, name:"PowerPlug1", type:21})
+  registerDevice({snr: 01399553, name:"PowerPlug2", type:21})
+  registerDevice({snr: 01185462, name:"Handsender", type:07})
+
   return;
   if (forceDevices && forceDevices.length) {
     forceDevices.forEach(element => {
